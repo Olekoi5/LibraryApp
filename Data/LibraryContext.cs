37970329975728c6d5778066aa4699ea5c113847
Gondocha234@outlook.com <Gondocha234@outlook.com>
2021-11-01 16:22:58 +0100

    Dodaj pliki projektów.

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using LibraryApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int id { get; set; }
+
+        [DisplayName("Tytuł")]
+        public string Title { get; set; }
+
+        [DisplayName("Autor")]
+        public string Author { get; set; }
+
+        [DisplayName("Data wydania")]
+        [DataType(DataType.Date)]
+        public DateTime DateRelease { get; set; }
+
+        [DisplayName("Krótki opis")]
+        [StringLength(50)]
+        public string ShortDiscription { get; set; }
+    }
+}
